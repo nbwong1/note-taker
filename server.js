daa21b0eead988d849126d7e9e9a138f1468bb89
nbwong <nicholas.b.wong@gmail.com>
2022-08-30 21:23:22 -0700

    added additional code for the POST of the new review

--- server.js
+++ server.js
@@ -1,22 +1,51 @@
 // use express, path, and PORT.
-const express = require('express');
-const { appendFile } = require('fs');
-const path = require('path');
-const noteData = require('./db/db.json');
-const PORT = 3001; 
+const express = require("express");
+const { appendFile } = require("fs");
+const path = require("path");
+const noteData = require("./db/notes.json");
+const PORT = 3000;
 
+const app = express();
 // set up express app to handle data parsing
-
-app.use(express.urlendcoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // get the index.html
-app.get('/'. (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'))
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
 //get notes.html
+app.get("/api/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "notes.html"));
+});
 
-app.get('/api/notes', (req, res) => res.json(petData));
+app.listen(PORT, () => {
+  console.log(`Example app listening at http://localhost:${PORT}`);
+});
+
+// post request to add note
+app.post("/api/notes", (req, res) => {
+  console.info(`${req.method} request received to add note`);
+
+  const { title, text } = req.body;
+
+  if (title && text) {
+    const newNote = {
+      title,
+      text,
+    };
+
+    const response = {
+      status: "success",
+      body: newNote,
+    };
+
+    console.log(response);
+    res.status(201).json(response);
+  } else {
+    res.status(500).json("Error in posting note");
+  }
+});
